4d48e4dead13ddb113a1c8a7bd82432c9bb951ac
Mai Huy Hoang <maihuyhoang97@gmail.com>
2022-01-30 14:01:35 +0700

    feat: finish webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -18,7 +18,19 @@ void get_URL(const string &host, const string &path) {
     // the "eof" (end of file).
 
     cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+
+    auto address = Address(host, "http");
+    TCPSocket sock;
+    sock.connect(address);
+    string message = "GET " + path + " HTTP/1.1\r\n"
+                     "Host: " + host + "\r\n"
+                     "Connection: close\r\n"
+                     "\r\n";
+    sock.write(message);
+    while (!sock.eof()) {
+        cout << sock.read();
+    }
+    sock.close();
 }
 
 int main(int argc, char *argv[]) {
